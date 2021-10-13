242b35e9648b0adae33e76dbfb17fa07c82ea98e
Artem Jelezcov <artem.jelezcov@simbirsoft.com>
2021-10-13 19:13:41 +0300

    add theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
-import React, { FC } from "react";
+import React, { FC } from 'react'
+import { ThemeProvider } from '@emotion/react'
+import { theme } from './theme'
+import styled from '@emotion/styled'
+
+const Block = styled.div`
+  width: 100px;
+  height: 100px;
+  background: ${({ theme }) => theme.gradients[0]};
+`
 
 export const App: FC = () => {
-  return <div className="App">aaa</div>;
-};
+  return (
+    <ThemeProvider theme={theme}>
+      <Block />
+    </ThemeProvider>
+  )
+}
---
+++ src/theme/theme.ts
@@ -0,0 +1,7 @@
+const gradients = {
+  0: "linear-gradient(255.35deg, #DC3131 0.83%, rgba(255, 79, 79, 0) 108.93%), #FF5E56",
+};
+
+export const theme = {
+  gradients,
+};
